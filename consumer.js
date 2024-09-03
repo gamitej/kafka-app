fd6518947402aa7ae4d7146322305628881439ff
gamitej <gamitejpratapsingh998@gmail.com>
2024-09-03 12:28:59 +0530

    NEW : consumer setup done with producer

---
+++ consumer.js
@@ -0,0 +1,20 @@
+const { kafka } = require("./client");
+
+async function init() {
+  const consumer = kafka.consumer({ groupId: "user-1" });
+  await consumer.connect();
+
+  await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+      console.log(
+        `[${topic} partition:${partition}] 
+        )}`,
+        message.value.toString()
+      );
+    },
+  });
+}
+
+init();
